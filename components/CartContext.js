54665b19a612daf4a5279b077f85393489419dcb
Ameer Fields <ameersf0@gmail.com>
2023-08-20 19:29:03 -0400

    Removed the comments on CartContext.js and added cartProducts,ls and ls and the end of both useEffect hooks

--- components/CartContext.js
+++ components/CartContext.js
@@ -5,11 +5,7 @@ export const CartContext = createContext({});
 export function CartContextProvider({children}) {
   const ls = typeof window !== "undefined" ? window.localStorage : null;
   const [cartProducts, setCartProducts] = useState([])
-  // useEffect(() => {
-  //   if(cartProducts?.length > 0) {
-  //     ls.setItem('cart', JSON.stringify(cartProducts))
-  //   }
-  // }, [cartProducts]);
+
   useEffect(() => {
     if (ls) {
       if (cartProducts?.length > 0) {
@@ -18,16 +14,18 @@ export function CartContextProvider({children}) {
         ls.removeItem('cart');
       }
     }
-  }, [cartProducts]);
+  }, [cartProducts, ls]);
+
   useEffect(() => {
     if(ls && ls.getItem('cart')) {
       setCartProducts(JSON.parse(ls.getItem('cart')))
     }
+  }, [ls]);
 
-  }, [])
   function addProduct(productId){
-    setCartProducts(prev => [...prev,productId]);
+    setCartProducts(prev => [...prev, productId]);
   }
+
   function removeProduct(productId){
     setCartProducts(prev => {
       const positionOfId = prev.indexOf(productId);
@@ -43,6 +41,8 @@ export function CartContextProvider({children}) {
   }
 
   return (
-    <CartContext.Provider value={{cartProducts, setCartProducts, addProduct, removeProduct, clearCart}}>{children}</CartContext.Provider>
+    <CartContext.Provider value={{cartProducts, setCartProducts, addProduct, removeProduct, clearCart}}>
+      {children}
+    </CartContext.Provider>
   )
 }
